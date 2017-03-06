d0ffef8ce02ed45cfa99d08abe6bc54a6929d80d
lieryang <lieryang@gujiabindeMacBook-Pro.local>
2017-03-06 13:07:45 +0800

    增加UILabel分类
    增加UILabel分类

---
+++ CategoryDemo/CategoryDemo/Category/UILabel+EYAddition.m
@@ -0,0 +1,24 @@
+//
+//  UILabel+EYAddition.m
+//  CategoryDemo
+//
+//  Created by lieryang on 17/3/6.
+//  Copyright © 2017年 lieryang. All rights reserved.
+//
+
+#import "UILabel+EYAddition.h"
+
+@implementation UILabel (EYAddition)
++ (instancetype)ey_labelWithText:(NSString *)text fontSize:(CGFloat)fontSize color:(UIColor *)color {
+    UILabel *label = [[self alloc] init];
+    
+    label.text = text;
+    label.font = [UIFont systemFontOfSize:fontSize];
+    label.textColor = color;
+    label.numberOfLines = 0;
+    
+    [label sizeToFit];
+    
+    return label;
+}
+@end
