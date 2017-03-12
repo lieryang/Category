b4dace848dc091cdeca9e0b217bb72304661558c
lieryang <lieryangios@126.com>
2017-03-12 21:45:15 +0800

    添加NSObject的分类
    添加NSObject的分类

---
+++ Category/NSArray+Log.m
@@ -0,0 +1,41 @@
+//
+//  NSArray+Log.m
+//  CategoryDemo
+//
+//  Created by lieryang on 17/3/6.
+//  Copyright © 2017年 lieryang. All rights reserved.
+//
+
+#import "NSArray+Log.h"
+
+@implementation NSArray (Log)
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"(\n"];
+    
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [strM appendFormat:@"\t%@,\n", obj];
+    }];
+    
+    [strM appendString:@")"];
+    
+    return strM;
+}
+
+@end
+
+@implementation NSDictionary (Log)
+
+- (NSString *)descriptionWithLocale:(id)locale
+{
+    NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+    
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [strM appendFormat:@"\t%@ = %@;\n", key, obj];
+    }];
+    
+    [strM appendString:@"}\n"];
+    
+    return strM;
+}
+@end
